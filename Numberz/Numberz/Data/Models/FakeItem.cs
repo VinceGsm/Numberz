2ba61f5367fa418b9572cdfa509a97b132f82e00
VinceGsm <v.gusmini@outlook.fr>
2024-02-18 18:22:19 +0100

    init Blazor + Identity

---
+++ Numberz/Numberz/Data/Models/FakeItem.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Numberz.Data.Models
+{
+    public class FakeItem
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ItemId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [MaxLength(1000)]        
+        public string? Description { get; set; }
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+
+        public DateTime? UpdateDate { get; set; }
+    }
+}
